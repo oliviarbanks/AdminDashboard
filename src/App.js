53240acba98f4556952f1be895d5b98ad6d72ebe
Olivia <oliviarbanks@gmail.com>
2023-09-15 10:28:18 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import { useState } from "react";
+import axios from 'axios';
+
+function App() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(""); // Initialize as an empty string
+  const [amount, setAmount] = useState(0);
+  const [paid, setPaid] = useState("");
+
+  const addAmbassador = () => {
+    // No need for date parsing/formatting
+    console.log(name);
+
+    // Parse the amount as a float
+    const parsedAmount = parseFloat(amount);
+
+    axios
+      .post('http://localhost:3001/create', {
+        name: name,
+        date: date, // Use the date as is, it's already in 'YYYY-MM-DD' format
+        amount: parsedAmount,
+        paid: paid,
+      })
+      .then(() => {
+        console.log('success');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input type="text" onChange={(event) => setName(event.target.value)} />
+        <label>Date:</label>
+        <input type="date" onChange={(event) => setDate(event.target.value)} />
+        <label>Amount:</label>
+        <input type="number" onChange={(event) => setAmount(event.target.value)} />
+        <label>Paid:</label>
+        <input type="text" onChange={(event) => setPaid(event.target.value)} />
+        <button onClick={addAmbassador}>
+          Add Earnings
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
